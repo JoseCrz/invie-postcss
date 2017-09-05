9a98fa93b2fd30521952fe8435fac35cfd40825d
Jose Cuevas <cueramjose@gmail.com>
2017-09-05 10:11:47 -0500

    6: Se agrego el plugin Autoprefixer de PostCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp")
 var postcss = require("gulp-postcss")
 var browserSync = require ("browser-sync").create()
+var autoprefixer = require("autoprefixer")
 
 //servidor de desarrollo
 
@@ -14,7 +15,9 @@ gulp.task("serve", function () {
 
 //Procesar CSS
 gulp.task("css", function () {
-  var processors = []
+  var processors = [
+    autoprefixer({ browsers: ["> 5%", "ie 8"]})
+  ]
 
   return gulp.src("./src/*.css")
   .pipe(postcss(processors))

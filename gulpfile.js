cc79ffb2e49477181516a6292cd540395aee3f98
Jose Cuevas <cueramjose@gmail.com>
2017-09-11 15:24:59 -0500

    14: Agregado Plugin de Mixins

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp")
 var postcss = require("gulp-postcss")
 var browserSync = require ("browser-sync").create()
 var cssnested = require("postcss-nested")
+var mixins = require ("postcss-mixins")
 var cssnext = require("postcss-cssnext")
 
 
@@ -18,6 +19,7 @@ gulp.task("serve", function () {
 //Procesar CSS
 gulp.task("css", function () {
   var processors = [
+    mixins(),
     cssnested,
     cssnext({ browsers: ["> 5%", "ie 8"]})
   ]
